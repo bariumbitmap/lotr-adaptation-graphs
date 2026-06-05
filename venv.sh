14f5db757dd52703d84df3b52fe7bb9818f399a9
bariumbitmap <bariumbitmap@gmail.com>
2026-06-05 14:32:24 -0400

    Work with generic shells.

--- venv.sh
+++ venv.sh
@@ -1,9 +1,9 @@
 if test -d .venv/bin
 then
-    source ./.venv/bin/activate
+    . ./.venv/bin/activate
 elif test -d .venv/Scripts
 then
-    source ./.venv/Scripts/activate
+    . ./.venv/Scripts/activate
 else
     printf "Error: cannot find virtual environment.\n" >&2
 fi
