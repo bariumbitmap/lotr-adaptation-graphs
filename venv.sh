60717d5f606a6de0f3764b6802e3eba0abbc5d9c
bariumbitmap <bariumbitmap@gmail.com>
2026-06-05 14:24:44 -0400

    Work on Windows also.

--- venv.sh
+++ venv.sh
@@ -1,1 +1,9 @@
-source ./.venv/bin/activate
+if test -d .venv/bin
+then
+    source ./.venv/bin/activate
+elif test -d .venv/Scripts
+then
+    source ./.venv/Scripts/activate
+else
+    printf "Error: cannot find virtual environment.\n" >&2
+fi
